2f505e1d60c11897071e21d46d55621de2e9566f
Afsal <afsalat9@gmail.com>
2023-12-28 17:34:55 +0530

    my skill -section is almost completed

--- src/pages/Home/HomeScreen/index.jsx
+++ src/pages/Home/HomeScreen/index.jsx
@@ -1,9 +1,12 @@
 import HeroSection from "../HeroSection";
+import Myskill from "../Myskill";
+
 
 export default function Home(){
     return(
         <>
         <HeroSection/>
+        <Myskill/>
         </>
     )
 }
---
+++ src/pages/Home/Myskill.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import data from "../../data/index.json";
+
+function Myskill() {
+  return (
+    <div className="skills--section" id="mySkills">
+      <div className="protfolio--continer">
+        <p className="section---title">My Skills</p>
+        <h2 className="skills--section--heading">My Expertise</h2>
+      </div>
+      <div className="skills--section--container">
+          {console.log(data)}
+        {data?.skills?.map((item, index) => (
+          <div key={index} className="skills--section--card">
+            <div className="skills--section--img">
+              <img src={item.src} alt="product chain"  />
+            </div>
+            <div className="skills--section--card--content">
+                <h3 className="skills--section--title">{item.title}</h3>
+                <p className="skills--section--description">{item.description}</p>
+            </div>
+          </div>
+        ))};
+      </div>
+    </div>
+  );
+}
+
+export default Myskill;
